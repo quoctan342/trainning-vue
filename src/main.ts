9d1e405a71997b5038ccda9b8d77d558ece74892
quoctan342 <quoctan342@gmail.com>
2022-10-07 09:57:32 +0700

    update library

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import './styles/scss/main.scss';
-import Vuelidate from 'vuelidate'
+import Vuelidate from 'vuelidate';
 
-Vue.config.productionTip = false
-Vue.use(Vuelidate)
+Vue.config.productionTip = false;
+
+Vue.use(Vuelidate);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount('#app');
