15a9a16d5522ea9dd3724f1c7561cdbaba315604
quoctan342 <quoctan342@gmail.com>
2022-10-21 11:06:05 +0700

    Update api

--- src/api/jokeAPI.ts
+++ src/api/jokeAPI.ts
@@ -1,23 +1,23 @@
-import * as api from './apiClient';
+import * as api from "./apiClient";
 
-const jokeAPI = {
-    getJoke() {
-        return api.get('', {});
-    },
-    getJokeByID(id: string | number) {
-        const path = `j/${id}`;
-        return api.get(path, {});
-    },
-    getJokeByWords(words: string) {
-        const path = 'search';
-        return api.get(path, {
-            params: {
-                page: 1,
-                limit: 30,
-                term: words,
-            },
-        });
-    },
+const jokeAPI: Object = {
+  getJoke() {
+    return api.get("", {});
+  },
+  getJokeByID(id: string) {
+    const path: string = `j/${id}`;
+    return api.get(path, {});
+  },
+  getJokeByWords(words: string) {
+    const path: string = "search";
+    return api.get(path, {
+      params: {
+        page: 1,
+        limit: 30,
+        term: words,
+      },
+    });
+  },
 };
 
 export default jokeAPI;
